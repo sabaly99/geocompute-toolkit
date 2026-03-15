f7a447006d5be26e8c9fb6d93f930b09ac93b5c5
Sabaly99 <naniasabaly99@gmail.com>
2026-03-15 16:31:22 +0000

    Second commit

--- geocompute/traverse_computation/traverse_comp.py
+++ geocompute/traverse_computation/traverse_comp.py
@@ -1,30 +1,45 @@
 import math 
 
+
+
 def trav_comp():
 
-    stations = float(input("ENTER THE NUMBER OF OBSERVATIONS MADE: "))
+    stations = int(input("ENTER THE NUMBER OF OBSERVATIONS MADE: "))
     BEARING = float(input("ENTER THE BEARING OF THE DEPARTURE LINE: "))
 
 
     def back_bear():
 
-    bearing = float(input("enter the bearing: "))
-    N_bearing = bearing % 360
+        bearing = float(input("enter the bearing the previous line: "))
+        N_bearing = bearing % 360
 
-    if N_bearing > 180:
-        new_bear = N_bearing - 180
-    elif:
-        new_bear = N_bearing + 180
+        if N_bearing > 180:
+            new_bear = N_bearing - 180
+        elif N_bearing < 180:
+            new_bear = N_bearing + 180
+        else:
+            return 0
 
-    return(new_bear)
-
-    BACK_BEARING = back_bear
+        return(new_bear)
 
 
+    
 
-    for i in  range(stations + 1):
+
+    for i in range(1 , stations + 1):
+
+        BACK_BEARING = back_bear()
+
 
         included_angle = float(input(f"ENTER THE INCLUDED ANGLE FOR obs{i}: "))
-        CALCULATED_BEARING = included_angle + 
+        BACK_BEARING = included_angle + BACK_BEARING
+
+
+        print(f"BEARING OF LINE {i} : {BACK_BEARING} degrees")
+
+
+
+trav_comp()
+
 
         
